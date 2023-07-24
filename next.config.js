8928be1199d2a48f52e51b4948a5d4d1929338a1
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-07-24 17:50:48 -0600

    SSR compatible solution

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['@dashincubator/secp256k1', 'dashkeys'],
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/client.js
+++ src/app/client.js
@@ -1,31 +1,22 @@
 'use client';
 
-import { useState, } from 'react'
-import Script from 'next/script'
+import { useState, useEffect } from 'react';
+import DashKeys from 'dashkeys'
 
 export default function ClientComponent() {
   const [wif, setWif] = useState(null)
 
+  useEffect(() => {
+    (async () => {
+      console.log('DashKeys', DashKeys)
+      let WIF = await DashKeys?.utils?.generateWifNonHd()
+      setWif(WIF)
+      console.log('DashKeys wif', WIF)
+    })()
+  }, [])
+
   return (
     <>
-      <Script
-        src="https://unpkg.com/@dashincubator/secp256k1@1.x/secp256k1.js"
-        onLoad={() => {
-          console.log('secp256k1 loaded')
-        }}
-      />
-      <Script
-        src="https://unpkg.com/dashkeys@1.x/dashkeys.js"
-        onLoad={() => {
-          let wifData
-          console.log('DashKeys loaded', window.DashKeys)
-          window.DashKeys.utils.generateWifNonHd()
-            .then(data => {
-              setWif(data)
-              console.log('DashKeys wif', data)
-            })
-        }}
-      />
       <h1>{wif}</h1>
     </>
   );
